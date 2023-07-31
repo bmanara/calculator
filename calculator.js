6f5dfa3842451c747ebc084312eb679f898a02a4
bmanara <65152569+bmanara@users.noreply.github.com>
2023-07-31 10:15:25 +0800

    Add basic calculator functions

---
+++ calculator.js
@@ -0,0 +1,19 @@
+function add(a, b) {
+    // Returns the sum of a and b passed to the function
+    return a + b;
+}
+
+function subtract(a, b) {
+    // Returns the difference of a and b passed to the function
+    return a - b;
+}
+
+function multiply(a, b) {
+    // Returns the product of a and b passed to the function
+    return a * b;
+}
+
+function divide(a, b) {
+    // Returns the quotient of a and b passed to the function
+    return a / b;
+}
